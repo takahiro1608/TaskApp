8591460eb0645d89b09549b6dda78a43a9e1d2f8
鈴木貴広 <takahiro.suzuki@gmail.com>
2017-01-16 18:52:14 +0900

    Storyboard上にカテゴリーのUILabel, UITextFieldを追加し、outlet接続。 Task.swift内のtaskクラスにcategory: String?プロパティを追加（正解かは不明...）

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Takahiro Suzuki on 2016/12/09.
+//  Copyright © 2016年 Takahiro Suzuki. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    // 日時
+    dynamic var date = NSDate()
+    
+    // カテゴリー
+    dynamic var category:String? = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
